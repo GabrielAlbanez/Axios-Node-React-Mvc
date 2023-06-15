2fcadc385a39f0c0129a6073e9851938351eec3c
GABRIEL GONCALVES ALBANEZ <gabriel.galbanez@senacsp.edu.br>
2023-06-15 17:08:10 -0300

    att react-native

--- crud-javascript-Albanez/mobile/App.js
+++ crud-javascript-Albanez/mobile/App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { Alert, FlatList, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import axios from "axios";
 import { useEffect, useState } from "react";
 export default function App() {
-  const [usuarios, setUsuarios] = useState([]);
   const url = "http://10.112.240.199:30000/usuarios";
+  const [usuarios, setUsuarios] = useState([]);
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [senha, setSenha] = useState("")
+  const [modalVisible, setModalVisible] = useState(false)
+  const [upadateModalVisible, setUpdateModalVisible] = useState(false)
+  const [selectUser, setSelectUSer] = useState(null)
 
-  useEffect(() => {
-    pegarUsuarios();
-  }, []);
 
-  const renderItem = ({ item }) => (
-    <View style={styles.itemContainer}>
-      <Text style={styles.itemName}>{item.email}</Text>
-      <Text style={styles.itemEmail}>{item.name}</Text>
-    </View>
-  );
   const pegarUsuarios = async () => {
     axios
       .get(url)
@@ -24,14 +21,199 @@ export default function App() {
       })
       .catch((error) => console.log(error));
   };
+
+  const limparCampos = () => {
+    setNome("")
+    setEmail("")
+    setSenha("")
+  }
+
+  const auxiliadorDeDeletacao = (id) => {
+    Alert.alert(
+      'Confimer',
+      'Tem certeza que deseja excluir esse Usuario?',
+      [{
+        text: 'Sim',
+        onPress: () => deletarUsuario(id)
+      },
+      {
+        text: 'não',
+        style: 'cancel'
+      }
+      ]
+    )
+  }
+
+  const deletarUsuario = (id) => {
+    axios.delete(`${url}/${id}`)
+      .then(() => {
+        const usuariosAtualizados = usuarios.filter((usuario) => usuario.id !== id)
+        setUsuarios(usuariosAtualizados)
+      })
+      .catch((errors) => { console.log(errors) })
+  }
+
+
+
+  const criarNovoUsuario = () => {
+    axios.post(url, {
+      name: nome,
+      email: email,
+      password: senha
+    })
+      .then(resposta => {
+        console.log(resposta.data)
+        pegarUsuarios()
+        setModalVisible(false)
+        limparCampos()
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  const autalilzarUser = (usuarioAutualizado) => {
+    const data = {
+      name: nome || usuarioAutualizado.nome,
+      email: email || usuarioAutualizado.email,
+      password: senha || usuarioAutualizado.password
+    }
+
+    axios.put(`${url}/${usuarioAutualizado.id}`, data)
+      .then((responsta) => {
+        console.log(responsta.data);
+        setUsuarios(
+          usuarios.map((item) => {
+            if (item.id === usuarioAutualizado.id) {
+              return {
+                ...item,
+                name: data.name,
+                email: data.email
+
+              }
+            }
+            return item
+          })
+        )
+        setUpdateModalVisible(false)
+        setNome("")
+        setEmail("")
+      })
+      .catch((errors) => {
+        console.log(errors)
+      })
+  }
+
+  useEffect(() => {
+    pegarUsuarios();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <View style={styles.itemContainer}>
+      <Text style={styles.itemName}>{item.name}</Text>
+      <Text style={styles.itemEmail}>{item.email}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          setUpdateModalVisible(true)
+          setSelectUSer(item)
+          setNome(item.name)
+          setEmail(item.email)
+        }}
+      >
+        <Feather name='edit' size={24} color={"#888"} />
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={() => auxiliadorDeDeletacao(item.id)}>
+        <Feather name='trash' size={24} color="#888" />
+      </TouchableOpacity>
+    </View>
+  );
+
+
+
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Lista De Usuarios</Text>
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Adicionar Usuario</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nome"
+            value={nome}
+            onChangeText={text => setNome(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TouchableOpacity style={styles.button} onPress={criarNovoUsuario}>
+            <Text style={styles.buttonText}>Adicionar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={setModalVisible(false)}>
+            <Text style={styles.buttonText}>Cancelar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <Modal
+        visible={upadateModalVisible}
+        animationType="slide"
+        transparent={true}
+        onRequestClose={() => setUpdateModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Atualizar Usuario</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="nome"
+              value={nome}
+              onChangeText={text => setNome(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Email"
+              value={email}
+              onChangeText={text => setEmail(text)}
+            />
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                autalilzarUser(selectUser)
+              }}
+            >
+              <Text style={styles.buttonText}>Atualizar</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText} onPress={() => {
+                setUpdateModalVisible(false)
+                limparCampos()
+              }}>
+                Cancelar
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Usuarios</Text>
+        <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
+          <Feather name="plus" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+
+
       <FlatList
         data={usuarios}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
-        style={styles.flatList}
       />
     </View>
   );
@@ -40,14 +222,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
+    marginTop: 40,
     paddingHorizontal: 20,
-    backgroundColor: "#f0f0f0",
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  header:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:"center",
+    marginBottom:20,
+  },
+  headerText:{
+    fontSize:24,
+    fontWeight:"bold",
+    
   },
   flatList: {
     marginBottom: 20,
@@ -69,16 +261,18 @@ const styles = StyleSheet.create({
     color: "#999999",
   },
   addButton: {
-    backgroundColor: "#ff4500",
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    position: "absolute",
-    bottom: 20,
-    right: 20,
-    elevation: 5,
+    backgroundColor: 'blue',
+    // width: 60,
+    // height: 60,
+    // borderRadius: 30,
+    // alignItems: "center",
+    // justifyContent: "center",
+    // position: "absolute",
+    // bottom: 20,
+    // right: 20,
+    // elevation: 5,
+    padding:10,
+    borderRadius:5,
   },
   addButtonText: {
     fontSize: 24,
